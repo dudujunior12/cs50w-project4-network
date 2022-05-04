6249fe86c5749d5dbd992610e0e2831e974bbd90
dudujunior12 <dudu12310@hotmail.com>
2022-05-04 19:54:57 -0300

    update: improved style and div organization

--- network/forms.py
+++ network/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class CreatePost(forms.Form):
-    post_text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Post', 'class': 'form-control'}), label='')
+    post_text = forms.CharField(widget=forms.Textarea(attrs={'placeholder': 'Post', 'class': 'form-control', 'rows': '5'}), label='')
