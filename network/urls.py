91ec1a4a3521e30e1ecda0e76969cf43edaecbdb
dudujunior12 <dudu12310@hotmail.com>
2022-05-04 21:12:36 -0300

    feat: profile url

--- network/urls.py
+++ network/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("new-post", views.new_post, name="new-post"),
+    path("profile/<str:username>", views.profile, name="profile")
 ]
