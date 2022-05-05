a9be5d871c4512045e5de68947bb6456d62bdc9e
dudujunior12 <dudu12310@hotmail.com>
2022-05-04 21:37:09 -0300

    feat: user profile posts and follow

--- network/views.py
+++ network/views.py
@@ -11,10 +11,10 @@ from .models import *
 
 def index(request):
     form = CreatePost()
-    all_posts = Post.objects.all()
+    posts = Post.objects.all()
     return render(request, "network/index.html", {
         "form": form,
-        "all_posts": all_posts,
+        "posts": posts,
     })
 
 def new_post(request):
